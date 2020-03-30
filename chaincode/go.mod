ae607b629b9f5c690535b7e2dcb40b0c2583e1a4
vadiminshakov <vadiminshakov@gmail.com>
2020-03-30 15:05:14 +0300

    fix

--- chaincode/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/vadiminshakov/new-programming-model-tutorial/chaincode
-
-go 1.14
-
-require github.com/hyperledger/fabric-contract-api-go v1.0.0
--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
-ARG='{"Args":[]}'
+export ARG='{"Args":[]}'
 docker-compose -f docker-compose-simple.yaml up -d
 cd chaincode && GOPROXY=direct go mod vendor -v
 sudo chmod -R +0777 ../chaincode
-docker exec chaincode sh -c "go build -v -mod=vendor -o chaincode"
-docker exec chaincode sh -c "CORE_CHAINCODE_ID_NAME=mycc:0 CORE_PEER_TLS_ENABLED=false ./chaincode -peer.address peer:7052"
+docker exec chaincode sh -c "GO111MODULE=on go build -v -mod=vendor -o chaincode"
+docker exec chaincode ls
+docker exec -e CORE_CHAINCODE_ID_NAME=mycc:0 -e CORE_PEER_TLS_ENABLED=false chaincode sh -c "./chaincode -peer.address peer:7052"
 sleep 5
 docker exec cli sh -c "peer chaincode install -p /opt/gopath/src/chaincodedev/chaincode -n mycc -v 0"
 docker exec cli sh -c "peer chaincode instantiate -n mycc -v 0 -c '$(ARG)' -C myc"
